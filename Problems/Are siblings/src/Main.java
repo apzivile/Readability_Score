d6db3e303dace7323a1c670231c31e46fde29acd
apzivile <zivile.apo@gmail.com>
2020-06-25 13:24:21 +0100

    Readability score project ; files problems

---
+++ Problems/Are siblings/src/Main.java
@@ -0,0 +1,11 @@
+/*public boolean areSiblings(File f1,File f2){
+        String parent1=f1.getParent();
+        String parent2=f2.getParent();
+        if(parent1.equals(parent2)){
+
+        return true;
+        }
+        return false;
+        }
+
+ */
