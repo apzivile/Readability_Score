c4031dbfd0f95c9c0171153916c77a948cbd54a0
apzivile <zivile.apo@gmail.com>
2020-06-27 17:02:37 +0100

    additional ; stringBuilder problems

---
+++ Problems/Concat all strings without digits/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+class ConcatenateStringsProblem {
+
+    public static String concatenateStringsWithoutDigits(String[] strings) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String string : strings) {
+            stringBuilder.append(string);
+        }
+        return stringBuilder.toString().replaceAll("[0-9]", "");
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] strings = scanner.nextLine().split("\\s+");
+        String result = concatenateStringsWithoutDigits(strings);
+        System.out.println(result);
+    }
+}
